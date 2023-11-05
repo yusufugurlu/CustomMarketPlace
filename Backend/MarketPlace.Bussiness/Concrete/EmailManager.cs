6f88baf543921e9deafad853644b58481d8bbbed
halitcirik <halitcirik@gmail.com>
2023-11-05 14:35:34 +0300

    Update_eMailTemplate

--- Backend/MarketPlace.Bussiness/Concrete/EmailManager.cs
+++ Backend/MarketPlace.Bussiness/Concrete/EmailManager.cs
@@ -7,11 +7,14 @@ using MarketPlace.DataTransfer.ServiceResults;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MarketPlace.Bussiness.Concrete
 {
@@ -50,15 +53,14 @@ namespace MarketPlace.Bussiness.Concrete
                     mail.SubjectEncoding = Encoding.UTF8;
                     mail.BodyEncoding = Encoding.UTF8;
                     mail.IsBodyHtml = true;
-
-                    mail.Body = HttpUtility.HtmlDecode("<html><body><h3>URL: <a href='" + fullPath + "'> Şifre yenile</a> <H3></body></html>");
+                    var htmlContent = $@"<!DOCTYPE html><html><body style=""font-family: Arial, sans-serif; text-align: center;""><div style=""background-color: #f9f9f9; padding: 20px; border-radius: 5px; width: 50%; margin: 0 auto; margin-top: 50px;""><h2>ŞİFRE SIFIRLAMA</h2><p>Merhaba,</p><p>Şifrenizi sıfırlamak için aşağıdaki bağlantıyı tıklayın:</p><a href=""{fullPath}"" style=""display: inline-block; padding: 10px 20px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px; margin-top: 20px;"">Şifre Sıfırlama</a><p style=""margin-top: 20px;"">Eğer şifreyi sıfırlamak istemediyseniz, bu e-postayı dikkate almayınız.</p><p style='margin-top: 20px;'>İletişim için websitemiz <a href='http://www.example.com' target='_blank'>www.example.com</a> adresini ziyaret edebilir veya bize ulaşmak için 123 456 78 90 numaralı telefonu arayabilirsiniz.</p></div></body></html>";
+                    mail.Body = HttpUtility.HtmlDecode(htmlContent);
 
                     //Şifreyi ikili doğrulamaya açıp mail seçilip oradan application password alınmalıdır.
                     client.Credentials = new System.Net.NetworkCredential("yussuf.ugurluu@gmail.com", "iaujhttedztiipwc");
                     client.EnableSsl = true;
                     client.Port = 587;
                     client.Send(mail);
-
                 }
             }
             catch (Exception ex)
