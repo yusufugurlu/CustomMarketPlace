010ac7d54d0f33d21d728dc62978f4133835b0bd
yusuf uğurlu <yusuf_ugurlu2015@outlook.com>
2023-11-06 19:41:19 -0800

    Migration AddTableSystemParameter

---
+++ Backend/MarketPlace.DataAccess/Models/CustomMarketPlaceModels/SystemParameter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace.DataAccess.Models.CustomMarketPlaceModels
+{
+    public class SystemParameter : BaseModel
+    {
+        public string? SmtpHost { get; set; }
+        public string? SmtpUserName { get; set; }
+        public string? SmtpPassword { get; set; }
+        public int SmtpPort { get; set; }
+        public int MaximumWorkplaceNumber { get; set; }
+        public int MaximumWUserNumber { get; set; }
+    }
+}
