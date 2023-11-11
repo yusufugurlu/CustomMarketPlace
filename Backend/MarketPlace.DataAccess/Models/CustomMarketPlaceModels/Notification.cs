4b37652b5fe1e15aefa99cb9d8cc3420e8429525
yusuf uğurlu <yusuf_ugurlu2015@outlook.com>
2023-11-10 20:29:09 -0800

    Migration AddTableNotification

---
+++ Backend/MarketPlace.DataAccess/Models/CustomMarketPlaceModels/Notification.cs
@@ -0,0 +1,17 @@
+﻿using MarketPlace.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace.DataAccess.Models.CustomMarketPlaceModels
+{
+    public class Notification : BaseModel
+    {
+        public string Message { get; set; }
+        public NotificationType NotificationType { get; set; }
+        public int UserId { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
